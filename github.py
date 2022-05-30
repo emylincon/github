ff9b7727e0d9e7505c99ce44087160398aae3787
emylincon <emylincon@gmail.com>
2022-05-30 14:26:50 +0100

    write tests

--- github.py
+++ github.py
@@ -15,7 +15,8 @@ class Contributions:
     @staticmethod
     def get_date_range(start, end):
         if start:
-            start, end = start.strftime('%Y-%m-%dT%H:%M:%SZ'), end.strftime('%Y-%m-%dT%H:%M:%SZ')
+            start, end = start.strftime(
+                '%Y-%m-%dT%H:%M:%SZ'), end.strftime('%Y-%m-%dT%H:%M:%SZ')
             return f'(from: "{start}", to: "{end}")'
         else:
             return ""
@@ -56,7 +57,8 @@ class Contributions:
         return t.render(username=username, date_range=self.get_date_range(start, end))
 
     def get_query(self, username, start_date=None, end_date=None):
-        query = {"query": f"{self.get_query_data(username, start_date, end_date)}"}
+        query = {
+            "query": f"{self.get_query_data(username, start_date, end_date)}"}
         response = requests.post(self.url, json=query, headers=self.header)
         with open("res.json", "w") as f:
             json.dump(response.json(), f)
@@ -66,32 +68,45 @@ class Contributions:
 class Transform:
     def __init__(self, data):
         self.data = data
-        self.tf_data = None
+        self.tf_data = self.days_contribution()
 
     def days_contribution(self):
         data = []
         for week in self.data["data"]["user"]["contributionsCollection"]["contributionCalendar"]["weeks"]:
             for day in week["contributionDays"]:
-                data.append({"date": day["date"], "contribution": day["contributionCount"]})
+                data.append(
+                    {"date": day["date"], "contribution": day["contributionCount"]})
         df = pd.DataFrame(data)
         df.date = pd.to_datetime(df.date, infer_datetime_format=True)
-        df['day_name'] = [df.iloc[i].date.day_name() for i in range(len(df))]
-        self.tf_data = df
+        df['day'] = [df.iloc[i].date.day_name() for i in range(len(df))]
+        df['month'] = [df.iloc[i].date.month_name() for i in range(len(df))]
+        return df
 
     def most_contribution_day(self):
         id_max = self.tf_data.contribution.idxmax()
         return self.tf_data.iloc[id_max]
 
     def weekday_contributions(self):
-        return self.tf_data.groupby(by="day_name").sum().sort_values(by=['contribution'], ascending=False)
+        return self.tf_data.groupby(by="day").sum().sort_values(by=['contribution'], ascending=False)
 
     def most_weekday_contributions(self):
         df = self.weekday_contributions()
         return df.iloc[0]
+
+    def month_contributions(self):
+        return self.tf_data.groupby(by="month").sum().sort_values(by=['contribution'], ascending=False)
+
+    def most_month_contributions(self):
+        df = self.month_contributions()
+        return df.iloc[0]
+
+    def least_month_contributions(self):
+        df = self.month_contributions()
+        return df.iloc[-1]
 
 
 if __name__ == "__main__":
     obj = Contributions()
     dd = obj.get_query("emylincon")
     tr = Transform(dd)
-    tr.days_contribution()
+    print(tr.most_contribution_day())
--- test_file.py
+++ test_file.py
@@ -1,5 +1,8 @@
 import unittest
+
+from numpy import int64
 from Regression import BestModel
+from github import Contributions, Transform
 
 
 class TestBasic(unittest.TestCase):
@@ -36,6 +39,72 @@ class TestBasic(unittest.TestCase):
         p = my_obj.compute_best_model()
         result = p.predict([25])[0]
         self.assertIsInstance(result, float)
+
+
+class TestContribution(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        """:arg
+        this runs once at the start of test
+        """
+        cls.con = Contributions()
+
+    @classmethod
+    def tearDownClass(cls):
+        """:arg
+        this runs once after all test is completed
+        """
+        print("teardownClass")
+
+    def setUp(self):
+        """:arg
+        this runs before each test
+        """
+        pass
+
+    def tearDown(self):
+        """:arg
+        this runs after each test
+        """
+        pass
+
+    def test_contributions(self):
+        data = self.con.get_query("emylincon")
+        self.assertIsInstance(data, (list, dict))
+
+
+class TestTransform(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        """:arg
+        this runs once at the start of test
+        """
+        obj = Contributions()
+        data = obj.get_query("emylincon")
+        cls.trans = Transform(data)
+
+    @classmethod
+    def tearDownClass(cls):
+        """:arg
+        this runs once after all test is completed
+        """
+        print("teardownClass")
+
+    def setUp(self):
+        """:arg
+        this runs before each test
+        """
+        pass
+
+    def tearDown(self):
+        """:arg
+        this runs after each test
+        """
+        pass
+
+    def test_most_contribution_day(self):
+        data = self.trans.most_contribution_day()
+        self.assertIsInstance(data.contribution, int64)
 
 
 if __name__ == "__main__":
