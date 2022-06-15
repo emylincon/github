c4b232586ca2dd46750afed07f4492998ec89335
Emeka E Ugwuanyi <emylincon@gmail.com>
2022-06-15 22:09:48 +0100

    display month contribution (#30)

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
 import streamlit as st
-import numpy as np
-import pandas as pd
+import github
 
-dataframe = pd.DataFrame(
-    np.random.randn(10, 20),
-    columns=('col %d' % i for i in range(20)))
+con_obj = github.Contributions()
 
-st.dataframe(dataframe.style.highlight_max(axis=0))
+st.title("Github Analysis")
+st.write("This app gives a dashboard of your github contributions")
+
+gitname = st.sidebar.text_input(
+    label="Git Repository", placeholder="Enter github repo")
+
+data = con_obj.get_query(gitname)
+
+stat_obj = github.Statistics(data=data)
+
+month_stat = stat_obj.month_contributions()
+
+# st.write(month_stat)
+st.write("# Monthly Contributions Bar Chart")
+st.bar_chart(month_stat)
---
+++ notebooks/Notes.md
@@ -0,0 +1,9 @@
+# Streamlit How To
+* start a predefined hello world streamlit app
+```
+streamlit hello
+```
+* run a file
+```
+streamlit run app.py
+```
