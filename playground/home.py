a752e0102591bbff97b7578e28c94fe39413d654
emylincon <emylincon@gmail.com>
2022-06-17 07:51:54 +0100

    steamlit explore one

---
+++ playground/home.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+st.write("Here's our first attempt at using data to create a table:")
+df = pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+})
+st.write(df)
+
+st.dataframe(df)
+
+st.table(df)
+
+st.line_chart(df)
+
+st.bar_chart(df)
+
+map_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+st.map(map_data)
