eedfbc0df9650e9444a435aaec10bee5f18a0e54
emylincon <emylincon@gmail.com>
2022-06-24 22:59:47 +0100

    explore update

--- playground/home.py
+++ playground/home.py
@@ -22,3 +22,29 @@ map_data = pd.DataFrame(
     columns=['lat', 'lon'])
 
 st.map(map_data)
+
+x = st.slider('x')  # 👈 this is a widget
+st.write(x, 'squared is', x * x)
+
+st.text_input("Your name", key="name")
+
+# You can access the value at any point with:
+st.session_state.name
+
+if st.checkbox('Show dataframe'):
+    chart_data = pd.DataFrame(
+        np.random.randn(20, 3),
+        columns=['a', 'b', 'c'])
+
+    chart_data
+
+df = pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+})
+
+option = st.selectbox(
+    'Which number do you like best?',
+    df['first column'])
+
+'You selected: ', option
