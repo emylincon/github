96eba1834bf3191828d70936a8c3f5a5febeae66
emylincon <emylincon@gmail.com>
2022-06-25 22:14:41 +0100

    more exploration added

--- playground/home.py
+++ playground/home.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+import time
 
 st.write("Here's our first attempt at using data to create a table:")
 df = pd.DataFrame({
@@ -48,3 +49,18 @@ option = st.selectbox(
     df['first column'])
 
 'You selected: ', option
+
+
+st.sidebar.write('Starting a long computation...')
+
+# Add a placeholder
+latest_iteration = st.sidebar.empty()
+bar = st.sidebar.progress(0)
+
+for i in range(100):
+    # Update the progress bar with each iteration.
+    latest_iteration.text(f'Iteration {i+1}')
+    bar.progress(i + 1)
+    time.sleep(0.1)
+
+st.sidebar.write('...and now we\'re done!')
