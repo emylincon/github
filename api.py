d7a195c9fce3cd1d048f39952a9e7efd3e3234c3
emylincon <emylincon@gmail.com>
2022-12-18 18:26:10 +0000

    api typing

--- api.py
+++ api.py
@@ -1,28 +1,28 @@
 import json
-from flask import Flask, jsonify
+from flask import Flask, jsonify, Response
 from github import Contributions, Statistics
 
-app = Flask(__name__)
-con_obj = Contributions()
+app: Flask = Flask(__name__)
+con_obj: Contributions = Contributions()
 
-VERSION = "v1"
+VERSION: str = "v1"
 
 
 @app.route("/")
-def root():
+def root() -> Response:
     return jsonify({"api_version": VERSION})
 
 
 @app.route(f"/{VERSION}/<string:username>/contributions/day/<string:kind>", methods=["GET"])
-def day_contributions(username, kind):
+def day_contributions(username, kind) -> Response:
     data = con_obj.get_query(username)
     with open("emeka", "w") as file:
         json.dump(data, file, indent=4)
 
-    stat_obj = Statistics(data=data)
-    response = ""
+    stat_obj: Statistics = Statistics(data=data)
+    response: dict
     if kind == "most":
         response = stat_obj.most_contribution_day().to_dict()
     else:
-        response = f"kind '{kind}' is not supported"
+        response = {"error": f"kind '{kind}' is not supported"}
     return jsonify({"api_version": VERSION, "response": response})
