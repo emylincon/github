682405d093e07ce29c01061aabe9ae9c501da68f
emylincon <emylincon@gmail.com>
2022-12-26 16:21:22 +0000

    api dev

--- api.py
+++ api.py
@@ -7,12 +7,15 @@ con_obj: Contributions = Contributions()
 VERSION: str = "v1"
 
 
+@app.route(f"/{VERSION}")
+@app.route("/latest")
 @app.route("/")
 def root() -> Response:
     return jsonify({"api_version": VERSION})
 
 
 @app.route(f"/{VERSION}/<string:username>/contributions/day/<string:kind>", methods=["GET"])
+@app.route("/latest/<string:username>/contributions/day/<string:kind>", methods=["GET"])
 def day_contributions(username: str, kind: str) -> Response:
     data: dict = con_obj.get_query(username)
     stat_obj: Statistics = Statistics(data=data)
